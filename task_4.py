1578c9361e14ba938b476ca576676acb5fde4bf9
svetaepc <40168757+svetaepc@users.noreply.github.com>
2022-11-29 23:40:47 +0300

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -9,5 +9,18 @@
 11
 2
 10
-"""
 
+autopep8 ON
+"""
+n = int(input('Введите число: '))
+
+
+def conv_dec_to_bin(n):
+    bin_num = ''
+    while n > 1:
+        bin_num += str(n % 2)
+        n = n // 2
+    return bin_num[::-1]
+
+
+print(conv_dec_to_bin(n))
