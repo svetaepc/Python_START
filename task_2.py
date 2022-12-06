bbe4ebd38feeb9bdc16adab63f9f057a1e914d5c
svetaepc <40168757+svetaepc@users.noreply.github.com>
2022-12-06 20:49:06 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -7,3 +7,18 @@
 [2, 4, 6, 8]
 """
 
+from random import randint
+
+def create_list(size, m, n):
+    return [randint(m, n) for i in range(size)]
+
+def get_unic_value(list):
+    return [i for i in set(list)]
+
+size = 10
+m = 1
+n = 10
+
+origin = create_list(size, m, n)
+print(origin)
+print(get_unic_value(origin))
