fd228a922dcda627578aafd822994e8feb77d17f
svetaepc <40168757+svetaepc@users.noreply.github.com>
2022-12-16 15:31:41 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -5,4 +5,36 @@
 Ввод: значение типа <str>
 Вывод: значение числового типа данных
 """
+import re
+ 
+ 
+actions = {
+  "^": lambda x, y: str(float(x)**float(y)),
+  "*": lambda x, y: str(float(x) * float(y)),
+  "/": lambda x, y: str(float(x) / float(y)),
+  "+": lambda x, y: str(float(x) + float(y)),
+  "-": lambda x, y: str(float(x) - float(y))
+}
+ 
+priority_reg_exp = r"\((.+?)\)"
+action_reg_exp = r"(-?\d+(?:\.\d+)?)\s*\{}\s*(-?\d+(?:\.\d+)?)"
+ 
+ 
+ 
+def my_eval(expresion: str) -> str:
+ 
+    while (match := re.search(priority_reg_exp, expresion)):
+        expresion: str = expresion.replace(match.group(0), my_eval(match.group(1)))
+ 
+    for symbol, action in actions.items():
+        while (match := re.search(action_reg_exp.format(symbol), expresion)):
+            expresion: str = expresion.replace(match.group(0), action(*match.groups()))
+ 
+    return expresion
+ 
+ 
+exp = "(1 + 4) * (5 * (10 - 2)) / 5"
 
+# И сравним с eval()
+
+print(my_eval(exp), eval(exp))
